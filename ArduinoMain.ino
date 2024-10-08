0a048b15c3ca62af277b5c9438b70aef9fda8af4
stagger552 <turindwa552@gmail.com>
2024-10-08 14:00:30 +0200

    Werking arduino sending data

--- ArduinoMain.ino
+++ ArduinoMain.ino
@@ -3,25 +3,35 @@
 #include <SPI.h>
 
 // Replace with your TTN credentials
-static const u1_t PROGMEM DEVEUI[8] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0xAE, 0x7D }; // DevEUI
-static const u1_t PROGMEM APPEUI[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };         // Replace with your AppEUI
+static const u1_t PROGMEM DEVEUI[8] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0xAE, 0x7D };  // DevEUI
+static const u1_t PROGMEM APPEUI[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };  // Replace with your AppEUI
 static const u1_t PROGMEM APPKEY[16] = { 0x8D, 0xB8, 0x04, 0xA4, 0x4E, 0x3F, 0xC3, 0xFB,
                                          0x49, 0xDF, 0x16, 0xC0, 0x77, 0xE8, 0xEE, 0xD };  // AppKey
 
+// Define these functions to return your keys
+void os_getArtEui(u1_t* buf) {
+    memcpy_P(buf, APPEUI, 8);
+    
+}
 
+void os_getDevEui(u1_t* buf) {
+    memcpy_P(buf, DEVEUI, 8);
+}
 
+void os_getDevKey(u1_t* buf) {
+    memcpy_P(buf, APPKEY, 16);
+}
 // Device-specific configuration
 const lmic_pinmap lmic_pins = {
   .nss = 10,  // Chip Select pin
   .rxtx = LMIC_UNUSED_PIN,
-  .rst = 9,                          // Reset pin
-  .dio = { 2, 3, LMIC_UNUSED_PIN },  // DIO0, DIO1 pins
+  .rst = 9,   // Reset pin
+  .dio = {2, 3, LMIC_UNUSED_PIN},  // DIO0, DIO1 pins
 };
-// const char message[] = "Hello, LoRa!"; 
-const char message[] = "Hello, LoRa!"; // Example message
+
+const char message[] = "Hello, LoRa!";  // Example message
 
 void setup() {
-
   Serial.begin(9600);
   Serial.println(F("Starting..."));
 
@@ -29,23 +39,13 @@ void setup() {
   os_init();
   LMIC_reset();
 
-
-  // Set up the LMIC with your credentials
-  LMIC_setSession(0x1, DEVEUI, APPKEY, APPEUI);
-  LMIC_selectSubBand(1);        // Select sub-band (0-7 for EU868)
-  LMIC_setDrTxpow(DR_SF7, 14);  // Set data rate and transmit power
+  // Start OTAA join process
+  LMIC_startJoining();
 }
 
 void loop() {
-  // Prepare and send the data
-  Serial.println(F("Sending..."));
-  // LMIC_setTxData2(1, (uint8_t*)message, strlen(message), 0);
-  
-  delay(10000);  // Wait 10 seconds before sending again
-
   // Process events
   os_runloop_once();
-  // put your main code here, to run repeatedly:
 }
 
 // Function to handle LoRaWAN events
@@ -56,7 +56,6 @@ void onEvent(ev_t ev) {
       break;
     case EV_JOINED:
       Serial.println(F("Joined"));
-      // LMIC_setTxData2(1, (uint8_t*)message, strlen(message), 0);
       break;
     case EV_TXCOMPLETE:
       Serial.println(F("TX Complete"));
